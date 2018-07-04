df8ff56e5fccf8c2fc170dc7e6a41c9d4caa7457
Sveinung Rundhovde <ssru@statoil.com>
2018-07-04 12:00:45 +0200

    Add test for compressed matrix implementation

--- timeshift/test/timeshift.cpp
+++ timeshift/test/timeshift.cpp
@@ -196,3 +196,229 @@ TEST_CASE("3 vintages (tiny cubes)") {
         CHECK( linsys.b.isApprox( expectedb, 1e-5 ) );
     }
 }
+
+TEST_CASE("Linear algebra") {
+
+    SECTION("4 traces, 3x3 per trace") {
+        Eigen::MatrixXd vp_L( 12, 12 ); vp_L <<
+          0.1, 0.4,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+          0.4, 0.2, 0.5,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+            0, 0.5, 0.3,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+            0,   0,   0, 0.5, 0.2,   0,   0,   0,   0,   0,   0,   0,
+            0,   0,   0, 0.2, 0.4, 0.1,   0,   0,   0,   0,   0,   0,
+            0,   0,   0,   0, 0.1, 0.3,   0,   0,   0,   0,   0,   0,
+            0,   0,   0,   0,   0,   0, 0.1, 0.1,   0,   0,   0,   0,
+            0,   0,   0,   0,   0,   0, 0.1, 0.1, 0.1,   0,   0,   0,
+            0,   0,   0,   0,   0,   0,   0, 0.1, 0.1,   0,   0,   0,
+            0,   0,   0,   0,   0,   0,   0,   0,   0, 0.2, 0.1,   0,
+            0,   0,   0,   0,   0,   0,   0,   0,   0, 0.1, 0.2, 0.1,
+            0,   0,   0,   0,   0,   0,   0,   0,   0,   0, 0.1, 0.2;
+
+        Eigen::MatrixXd vp_L_c( 12, 2 ); vp_L_c <<
+          0.1, 0.4,
+          0.2, 0.5,
+          0.3,   0,
+          0.5, 0.2,
+          0.4, 0.1,
+          0.3,   0,
+          0.1, 0.1,
+          0.1, 0.1,
+          0.1,   0,
+          0.2, 0.1,
+          0.2, 0.1,
+          0.2,   0;
+
+        Eigen::MatrixXd Bnn( 3, 3 ); Bnn <<
+          1,4,0,
+          4,2,5,
+          0,5,3;
+
+        SECTION("2 vintages, no horizontal smoothing") {
+            Eigen::VectorXd x( 12 );
+            x << 1,2,3,4,5,6,7,8,9,10,11,12;
+
+            Eigen::MatrixXi comb( 1, 1 ); comb.setZero();
+
+            const auto expected = vp_L * x;
+
+            SECTION("2 inlines, 2 crosslines") {
+                BlockBandedMatrix< double > bbm( vp_L_c,
+                                                 2,
+                                                 Bnn.sparseView(),
+                                                 comb,
+                                                 2,
+                                                 2, 2 );
+                const auto result = bbm * x;
+                CHECK( result.isApprox( expected ) );
+            }
+
+            SECTION("4 inlines, 1 crosslines") {
+                BlockBandedMatrix< double > bbm( vp_L_c,
+                                                 2,
+                                                 Bnn.sparseView(),
+                                                 comb,
+                                                 2,
+                                                 4, 1 );
+                const auto result = bbm * x;
+                CHECK( result.isApprox( expected ) );
+            }
+
+            SECTION("1 inlines, 4 crosslines") {
+                BlockBandedMatrix< double > bbm( vp_L_c,
+                                                 2,
+                                                 Bnn.sparseView(),
+                                                 comb,
+                                                 2,
+                                                 1, 4 );
+                const auto result = bbm * x;
+                CHECK( result.isApprox( expected ) );
+            }
+        }
+
+        Eigen::VectorXd x( 24 );
+        x << 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24;
+
+        SECTION("3 vintages, no horizontal smoothing") {
+            Eigen::MatrixXi comb( 2, 2 ); comb.setZero();
+
+            Eigen::MatrixXd L( 24, 24 );
+            L.topLeftCorner( 12, 12 ) = vp_L;
+            L.bottomLeftCorner( 12, 12 ) = 2 * vp_L;
+            L.topRightCorner( 12, 12 ) = 3 * vp_L;
+            L.bottomRightCorner( 12, 12 ) = 4 * vp_L;
+
+            Eigen::MatrixXd L_c( 24, 4 );
+            L_c.topLeftCorner( 12, 2 ) = vp_L_c;
+            L_c.bottomLeftCorner( 12, 2 ) = 2 * vp_L_c;
+            L_c.topRightCorner( 12, 2 ) = 3 * vp_L_c;
+            L_c.bottomRightCorner( 12, 2 ) = 4 * vp_L_c;
+
+            const auto expected = L * x;
+
+            SECTION("2 inlines, 2 crosslines") {
+                BlockBandedMatrix< double > bbm( L_c,
+                                                 2,
+                                                 Bnn.sparseView(),
+                                                 comb,
+                                                 3,
+                                                 2, 2 );
+                const auto result = bbm * x;
+                CHECK( result.isApprox( expected ) );
+            }
+
+            SECTION("4 inlines, 1 crosslines") {
+                BlockBandedMatrix< double > bbm( L_c,
+                                                 2,
+                                                 Bnn.sparseView(),
+                                                 comb,
+                                                 3,
+                                                 4, 1 );
+                const auto result = bbm * x;
+                CHECK( result.isApprox( expected ) );
+            }
+
+            SECTION("1 inlines, 4 crosslines") {
+                BlockBandedMatrix< double > bbm( L_c,
+                                                 2,
+                                                 Bnn.sparseView(),
+                                                 comb,
+                                                 3,
+                                                 1, 4 );
+                const auto result = bbm * x;
+                CHECK( result.isApprox( expected ) );
+            }
+        }
+
+        SECTION("3 vintages, with horizontal smoothing") {
+            Eigen::MatrixXi comb( 2, 2 );
+            comb << 1,2,3,4;
+
+            Eigen::MatrixXd smoothing( 12,12 );
+            smoothing.setZero();
+
+            Eigen::MatrixXd L_c( 24, 4 );
+            L_c.topLeftCorner( 12, 2 ) = vp_L_c;
+            L_c.bottomLeftCorner( 12, 2 ) = 2 * vp_L_c;
+            L_c.topRightCorner( 12, 2 ) = 3 * vp_L_c;
+            L_c.bottomRightCorner( 12, 2 ) = 4 * vp_L_c;
+
+            SECTION("2 inlines, 2 crosslines") {
+                smoothing.block( 0, 0, 3, 3 ) = 2 * Bnn;
+                smoothing.block( 0, 3, 3, 3 ) = Bnn;
+                smoothing.block( 0, 6, 3, 3 ) = Bnn;
+
+                smoothing.block( 3, 3, 3, 3 ) = 2 * Bnn;
+                smoothing.block( 3, 0, 3, 3 ) = Bnn;
+                smoothing.block( 3, 9, 3, 3 ) = Bnn;
+
+                smoothing.block( 6, 6, 3, 3 ) = 2 * Bnn;
+                smoothing.block( 6, 9, 3, 3 ) = Bnn;
+                smoothing.block( 6, 0, 3, 3 ) = Bnn;
+
+                smoothing.block( 9, 9, 3, 3 ) = 2 * Bnn;
+                smoothing.block( 9, 6, 3, 3 ) = Bnn;
+                smoothing.block( 9, 3, 3, 3 ) = Bnn;
+
+                Eigen::MatrixXd L( 24, 24 );
+                L.topLeftCorner( 12, 12 ) = vp_L - comb(0,0)*smoothing;
+                L.bottomLeftCorner( 12, 12 ) = 2*vp_L - comb(1,0)*smoothing;
+                L.topRightCorner( 12, 12 ) = 3*vp_L - comb(0,1)*smoothing;
+                L.bottomRightCorner( 12, 12 ) = 4*vp_L - comb(1,1)*smoothing;
+
+                BlockBandedMatrix< double > bbm( L_c,
+                                                 2,
+                                                 Bnn.sparseView(),
+                                                 comb,
+                                                 3,
+                                                 2, 2 );
+                const auto expected = L * x;
+                const auto result = bbm * x;
+                CHECK( result.isApprox( expected ) );
+            }
+
+            smoothing.block( 0, 0, 3, 3 ) = 3 * Bnn;
+            smoothing.block( 0, 3, 3, 3 ) = Bnn;
+
+            smoothing.block( 3, 3, 3, 3 ) = 2 * Bnn;
+            smoothing.block( 3, 0, 3, 3 ) = Bnn;
+            smoothing.block( 3, 6, 3, 3 ) = Bnn;
+
+            smoothing.block( 6, 6, 3, 3 ) = 2 * Bnn;
+            smoothing.block( 6, 9, 3, 3 ) = Bnn;
+            smoothing.block( 6, 3, 3, 3 ) = Bnn;
+
+            smoothing.block( 9, 9, 3, 3 ) = 3 * Bnn;
+            smoothing.block( 9, 6, 3, 3 ) = Bnn;
+
+            Eigen::MatrixXd L( 24, 24 );
+            L.topLeftCorner( 12, 12 ) = vp_L - comb(0,0) * smoothing;
+            L.bottomLeftCorner( 12, 12 ) = 2 * vp_L - comb(1,0) * smoothing;
+            L.topRightCorner( 12, 12 ) = 3 * vp_L - comb(0,1) * smoothing;
+            L.bottomRightCorner( 12, 12 ) = 4 * vp_L - comb(1,1) * smoothing;
+
+            const auto expected = L * x;
+
+            SECTION("1 inlines, 4 crossline") {
+                BlockBandedMatrix< double > bbm( L_c,
+                                                2,
+                                                Bnn.sparseView(),
+                                                comb,
+                                                3,
+                                                1, 4 );
+                const auto result = bbm * x;
+                CHECK( result.isApprox( expected ) );
+            }
+
+            SECTION("4 inlines, 1 crossline") {
+                BlockBandedMatrix< double > bbm( L_c,
+                                                 2,
+                                                 Bnn.sparseView(),
+                                                 comb,
+                                                 3,
+                                                 4, 1 );
+                const auto result = bbm * x;
+                CHECK( result.isApprox( expected ) );
+            }
+        }
+    }
+}
