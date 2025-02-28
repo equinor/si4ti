501479ebabb9c1cc63804131a9a7b49a0b7c4306
Alexander Jaust <18347253+ajaust@users.noreply.github.com>
2025-02-28 09:15:33 +0100

    Update own build script

--- build-si4ti.sh
+++ build-si4ti.sh
@@ -15,10 +15,20 @@ rm -rf build
 mkdir build
 pushd build
 
-cmake ..  --log-level=VERBOSE -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_SHARED_LIBS=ON -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}" -DUSE_FFTW=ON
-
+cmake ..  --log-level=VERBOSE -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DBUILD_SHARED_LIBS=ON -DCMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}" -DUSE_FFTW=ON -DSI4TI_BUILD_TIMESHIFT=ON -DSI4TI_PYTHON_BINDINGS=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 make -j7
 make diff
+
+echo "Running cppcheck"
+cppcheck --enable=style,portability,performance,warning \
+	--library=posix \
+	--inline-suppr \
+	--project=compile_commands.json \
+	--error-exitcode=1 \
+	--suppressions-list=../cppcheck/suppressions.txt \
+	--xml \
+	--check-level=exhaustive
+
 make install
 
 popd
