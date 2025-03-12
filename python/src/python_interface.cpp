8906210f53a471d4a40c0c4a621e8f815be61f76
Alexander Jaust <18347253+ajaust@users.noreply.github.com>
2025-03-12 16:32:24 +0100

    fixup! Disable dynamic versioning

--- python/src/python_interface.cpp
+++ python/src/python_interface.cpp
@@ -192,5 +192,5 @@ PYBIND11_MODULE(_si4ti_python, m) {
         .def_readwrite("latsmooth_4D", &si4ti::python::ImpedanceOptions::latsmooth_4D)
         .def_readwrite("max_iter", &si4ti::python::ImpedanceOptions::max_iter);
 
-    m.attr("__version__") = "1.1.0.a3";
+    m.attr("__version__") = "1.1.0a3";
 }
