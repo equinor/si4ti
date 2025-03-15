f39573731dc5cb8b7039ea040088f06d3fc9557e
Alexander Jaust <18347253+ajaust@users.noreply.github.com>
2025-03-15 17:18:50 +0100

    Trying to add xlinesorted tests

--- python/tests/test_impedance.py
+++ python/tests/test_impedance.py
@@ -16,6 +16,13 @@ def input_cubes() -> list[xtgeo.Cubes]:
     return [xtgeo.cube_from_file(filename) for filename in INPUT_FILES]
 
 
+@pytest.fixture
+def input_cubes_xlinesorted(input_cubes: list[xtgeo.Cubes]) -> list[xtgeo.Cubes]:
+    for c in input_cubes:
+        c.swapaxes()
+    return input_cubes
+
+
 def compare_cubes(
     actuals: list[xtgeo.Cubes],
     references: list[xtgeo.Cubes],
@@ -63,6 +70,30 @@ def test_timevarying_wavelet_default_options(input_cubes: list[xtgeo.Cubes]) -> 
     compare_cubes(dsyn_cubes, expected_dsyn_cubes)
 
 
+@pytest.mark.limit_memory("10.5 MB")
+def test_timevarying_wavelet_xlinesorted_default_options(
+    input_cubes_xlinesorted: list[xtgeo.Cubes],
+) -> None:
+    relAI_cubes, dsyn_cubes = compute_impedance(
+        input_cubes_xlinesorted, tv_wavelet=True
+    )
+
+    expected_relAI_cubes = [
+        xtgeo.cube_from_file(f"../test-data/imp-tvw-relAI-{i}-ref.sgy")
+        for i in range(3)
+    ]
+    for c in expected_relAI_cubes:
+        c.swapaxes()
+    expected_dsyn_cubes = [
+        xtgeo.cube_from_file(f"../test-data/imp-tvw-dsyn-{i}-ref.sgy") for i in range(3)
+    ]
+    for c in expected_dsyn_cubes:
+        c.swapaxes()
+
+    compare_cubes(relAI_cubes, expected_relAI_cubes)
+    compare_cubes(dsyn_cubes, expected_dsyn_cubes)
+
+
 @pytest.mark.limit_memory("9.8 MB")
 def test_timeinvariant_wavelet_default_options(input_cubes: list[xtgeo.Cubes]) -> None:
     relAI_cubes, dsyn_cubes = compute_impedance(input_cubes)
@@ -80,7 +111,35 @@ def test_timeinvariant_wavelet_default_options(input_cubes: list[xtgeo.Cubes]) -
     compare_cubes(dsyn_cubes, expected_dsyn_cubes)
 
 
-@pytest.mark.limit_memory("9.8 MB")
+@pytest.mark.limit_memory("9.6 MB")
+def test_timevarying_wavelet_segmented_xlinesorted(
+    input_cubes_xlinesorted: list[xtgeo.Cubes],
+) -> None:
+    relAI_cubes, dsyn_cubes = compute_impedance(
+        input_cubes_xlinesorted,
+        tv_wavelet=True,
+        segments=2,
+        max_iter=3,
+    )
+
+    expected_relAI_cubes = [
+        xtgeo.cube_from_file(f"../test-data/imp-segmented-relAI-{i}-ref.sgy")
+        for i in range(3)
+    ]
+    for c in expected_relAI_cubes:
+        c.swapaxes()
+    expected_dsyn_cubes = [
+        xtgeo.cube_from_file(f"../test-data/imp-segmented-dsyn-{i}-ref.sgy")
+        for i in range(3)
+    ]
+    for c in expected_dsyn_cubes:
+        c.swapaxes()
+
+    compare_cubes(relAI_cubes, expected_relAI_cubes)
+    compare_cubes(dsyn_cubes, expected_dsyn_cubes)
+
+
+@pytest.mark.limit_memory("9.6 MB")
 def test_timevarying_wavelet_segmented(input_cubes: list[xtgeo.Cubes]) -> None:
     relAI_cubes, dsyn_cubes = compute_impedance(
         input_cubes,
