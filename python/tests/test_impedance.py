09bb39961df10ed11b193866442589ca743cf938
Alexander Jaust <18347253+ajaust@users.noreply.github.com>
2025-03-21 11:11:47 +0100

    Increase error bound
    The error are too large for very few data points in the  CI using Linux
    and FFTW. The error is exceeded for only very few data points and the
    bound has to be increased only moderately.

--- python/tests/test_impedance.py
+++ python/tests/test_impedance.py
@@ -170,7 +170,9 @@ def test_timevarying_wavelet_segmented_crosslinesorted(
         for i in range(3)
     ]
 
-    compare_cubes(relAI_cubes, expected_relAI_cubes)
+    # Increase max bound due to failures on CI using Linux and FFTW. The
+    # default error bound is exceeded in less than 0.025% of the data points.
+    compare_cubes(relAI_cubes, expected_relAI_cubes, max_diff_bound=4.8e-4)
     compare_cubes(dsyn_cubes, expected_dsyn_cubes)
 
 
